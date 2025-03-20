6bb7414cec23a8cbceeb0ba32d9b4680bb3a6a31
lukeckprobierts <luka.simonovic07@gmail.com>
2025-03-21 00:26:39 +0100

    set up basic chatbot functionality using ollama, gemma3 and flask, still unrefined frontend and error handling.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response, stream_with_context
 from config import Config
+from ollama import chat
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route("/")
+def ChatBot_query(prompt):
+    stream = chat(
+        model='gemma3:27b',
+        messages=[{'role': 'user', 'content': prompt}],
+        stream=True,
+    )
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
+        yield f"{chunk['message']['content']}"
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        user_input = request.form['inputText']
+        return Response(stream_with_context(ChatBot_query(user_input)), content_type='text/event-stream')
     return render_template("index.html")
 
 if __name__ == "__main__":
